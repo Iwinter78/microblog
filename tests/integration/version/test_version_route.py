2c334460e460bf0396edb07747f770b4146dc680
Iwinter78 <kille1404.so@gmail.com>
2025-11-29 14:25:38 +0100

    chore: fixed syntax error

--- tests/integration/version/test_version_route.py
+++ tests/integration/version/test_version_route.py
@@ -1,5 +1,4 @@
-def test_fetch_version_of_server(client)
-
-response = client.get("/version", follow_redirects=True)
-assert response.status_code == 200
-assert b"version" in response.data
+def test_fetch_version_of_server(client):
+    response = client.get("/version", follow_redirects=True)
+    assert response.status_code == 200
+    assert b"version" in response.data
